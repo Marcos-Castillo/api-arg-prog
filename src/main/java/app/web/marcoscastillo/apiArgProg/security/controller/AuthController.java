b71ae1e8085426cd8d0a50180cd3bb17287a8344
Marcos-Castillo <93360137+Marcos-Castillo@users.noreply.github.com>
2022-08-28 21:44:37 -0300

    CrossOrigin
    url nueva cors

--- src/main/java/app/web/marcoscastillo/apiArgProg/security/controller/AuthController.java
+++ src/main/java/app/web/marcoscastillo/apiArgProg/security/controller/AuthController.java
@@ -28,7 +28,7 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("/auth")
-//@CrossOrigin
+@CrossOrigin("https://marcos-castillo.web.app/")
 public class AuthController {
 
     @Autowired
